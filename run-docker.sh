2da79003d8e82d1b0ddfb85ed6dd796a5723c1b7
docbull <fudghks21@gmail.com>
2022-07-09 21:15:40 +0900

    bash file for runinng test docker container

--- run-docker.sh
+++ run-docker.sh
@@ -3,6 +3,6 @@
 DOCKER_COMPOSE_FILE_BASE=./docker-compose/docker-compose-test.yaml
 DOCKER_COMPOSE_FILE="-f ${DOCKER_COMPOSE_FILE_BASE}"
 
-docker compose ${DOCKER_COMPOSE_FILE} up -d
+docker-compose ${DOCKER_COMPOSE_FILE} up -d
 
 docker ps
